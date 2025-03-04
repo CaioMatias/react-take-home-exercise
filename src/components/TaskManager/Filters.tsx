3bd7d061cb3a1c759d75b8f7b70463b5d228b414
Caio Matias <caiobellomatias@gmail.com>
2025-03-04 09:54:31 -0300

    Hotfix: DRY filters

--- src/components/TaskManager/Filters.tsx
+++ src/components/TaskManager/Filters.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { useTaskManagerStore } from "@/store";
 
+const filters = ["all", "completed", "pending"];
+
 const Filters = () => {
   const { activeFilter, setFilter } = useTaskManagerStore((state) => state);
 
@@ -11,30 +13,17 @@ const Filters = () => {
 
   return (
     <div className="flex gap-4 justify-around mb-4 text-gray-700">
-      <button
-        className={`grow rounded py-2 ${
-          activeFilter === "all" ? "bg-gray-200" : ""
-        }`}
-        onClick={handleFilterClick("all")}
-      >
-        All
-      </button>
-      <button
-        className={`grow rounded py-2 ${
-          activeFilter === "completed" ? "bg-gray-200" : ""
-        }`}
-        onClick={handleFilterClick("completed")}
-      >
-        Completed
-      </button>
-      <button
-        className={`grow rounded py-2 ${
-          activeFilter === "pending" ? "bg-gray-200" : ""
-        }`}
-        onClick={handleFilterClick("pending")}
-      >
-        Pending
-      </button>
+      {filters.map((filter) => (
+        <button
+          key={filter}
+          className={`grow rounded capitalize py-2 ${
+            activeFilter === filter ? "bg-gray-200" : ""
+          }`}
+          onClick={handleFilterClick(filter)}
+        >
+          {filter}
+        </button>
+      ))}
     </div>
   );
 };
