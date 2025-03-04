d9b3941ff3c99db4684c2dfd93b7b4d92535cbdb
Caio Matias <caiobellomatias@gmail.com>
2025-03-04 09:43:18 -0300

    Add UI Improvements (#3)

--- src/components/TaskManager/Item.tsx
+++ src/components/TaskManager/Item.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { createPortal } from "react-dom";
+
+import Modal from "@/components/TaskManager/Modal";
 
 import { useTaskManagerStore } from "@/store";
 import { Task } from "@/types";
 
-export type Props = {
+type Props = {
   task: Task;
 };
 
 const Item = ({ task }: Props) => {
+  const [showModal, setShowModal] = React.useState(false);
+
   const { deleteTask, toggleTaskCompleted } = useTaskManagerStore(
     (state) => state
   );
@@ -17,11 +22,18 @@ const Item = ({ task }: Props) => {
   };
 
   const handleDeleteClick = () => {
-    deleteTask(task.id);
+    setShowModal(true);
+  };
+
+  const handleDeleteConsent = () => {
+    setShowModal(false);
+    setTimeout(() => {
+      deleteTask(task.id);
+    }, 300);
   };
 
   return (
-    <li className="flex items-center justify-between border-b py-2">
+    <li className="flex items-center justify-between border-b py-4">
       <span
         className={`cursor-pointer ${
           task.completed ? "line-through text-green-500" : "text-black"
@@ -32,11 +44,20 @@ const Item = ({ task }: Props) => {
       </span>
 
       <button
-        className="bg-red-500 text-white px-4 rounded"
+        className="bg-red-500 text-white px-4 py-1 rounded"
         onClick={handleDeleteClick}
       >
         Delete
       </button>
+
+      {createPortal(
+        <Modal
+          isOpen={showModal}
+          onDelete={handleDeleteConsent}
+          onCancel={() => setShowModal(false)}
+        />,
+        document.body
+      )}
     </li>
   );
 };
--- src/components/TaskManager/List.tsx
+++ src/components/TaskManager/List.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AnimatePresence, LayoutGroup, motion } from "motion/react";
 
 import Item from "@/components/TaskManager/Item";
 
@@ -7,19 +8,38 @@ import { useTaskManagerStore } from "@/store";
 const List = () => {
   const { tasks, activeFilter } = useTaskManagerStore((state) => state);
 
+  const filteredTasks = tasks.filter((task) => {
+    if (activeFilter === "all") return true;
+    if (activeFilter === "completed") return task.completed;
+    if (activeFilter === "pending") return !task.completed;
+    return false;
+  });
+
   return (
-    <ul>
-      {tasks
-        .filter((task) => {
-          if (activeFilter === "all") return true;
-          if (activeFilter === "completed") return task.completed;
-          if (activeFilter === "pending") return !task.completed;
-          return false;
-        })
-        .map((task) => (
-          <Item key={task.id} task={task} />
-        ))}
-    </ul>
+    <motion.ul
+      className="overflow-hidden"
+      animate={{
+        height: `${filteredTasks.length * 65}px`,
+        transition: { delay: 0.15 },
+      }}
+      layout
+    >
+      <AnimatePresence>
+        <LayoutGroup>
+          {filteredTasks.map((task) => (
+            <motion.div
+              key={task.id}
+              initial={{ x: -100, opacity: 0 }}
+              exit={{ y: -20, opacity: 0 }}
+              animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}
+              layout
+            >
+              <Item task={task} />
+            </motion.div>
+          ))}
+        </LayoutGroup>
+      </AnimatePresence>
+    </motion.ul>
   );
 };
 
